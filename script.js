a222cb32a33095c3d0e84ad9674b176e37e858ed
Sergey13111 <boiko.sergey93@gmail.com>
2022-02-22 21:37:45 +0200

    added homeTask2

--- script.js
+++ script.js
@@ -10,6 +10,23 @@ function sumTwoSmallestNumbers(array, ...arg) {
     return sortedArray[0] + sortedArray[1];
 };
 
-console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77, 676, 888, 7, 8])); // 7
-console.log(sumTwoSmallestNumbers([1, 3, 2])); // 3
-console.log(sumTwoSmallestNumbers([1])); // undefined и отдельный console.error("...")
+console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77, 676, 888, 7, 8]));
+console.log(sumTwoSmallestNumbers([1, 3, 2]));
+console.log(sumTwoSmallestNumbers([1]));
+
+// 2
+const createCalculator = (firstVariable) => {
+    let value = 0;
+    return {
+        sum: (value) => (firstVariable += value),
+        mult: (value) => (firstVariable *= value),
+        sub: (value) => (firstVariable -= value),
+        div: (value) => (firstVariable /= value),
+    };
+};
+const calc = createCalculator(10);
+
+console.log(calc.sum(5));
+console.log(calc.mult(10));
+console.log(calc.sub(40));
+console.log(calc.div(10));
